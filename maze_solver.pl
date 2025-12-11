89e453cc08b58ea1adf8f2c28b94069a80f0dadf
Srikar <srikar.kavali@gmail.com>
2025-12-10 20:34:39 -0600

    Implemented more helper functions, particularly count starts, count exits, count cells, and find start. The idea is that by adding all these helper functions, coding the main logic portion of the project will become easier and will lead to less code that I have to write in logic portion since I can use these helper functions.

--- maze_solver.pl
+++ maze_solver.pl
@@ -14,3 +14,23 @@ is_rectangular([]).
 is_rectangualr([Row|Rows]) :-
     length(Row, Len),
     all_same_lengths(Rows, Len).
+
+count_starts(Maze, Count) :-
+    flatten(Maze, Cells),
+    count_cell(Cells, s, Count).
+
+count_exits(Maze, Count) :-
+    flatten(Maze, Cells),
+    count_cell(Cells, e, Count).
+
+count_cell([], _, 0).
+count_cell([Cell|Rest], Cell, Count) :-
+    !,
+    count_cell(Rest, Cell, RestCount),
+    Count is RestCount + 1.
+count_cell([_|Rest], Cell, Count) :-
+    count_cell(Rest, Cell, Count).
+
+find_start(Maze, Row, Col) :-
+    find_cell(Maze, s, 0, Row, Col).
+
